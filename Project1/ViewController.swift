82c738df5c7ae1e6bb87d75b748e6a3675fac6f7
samir-s-hassan <hsamir347@gmail.com>
2024-01-12 20:51:39 -0500

    added prework files to repository

---
+++ Project1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Samir Hassan on 1/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+            view.backgroundColor = randomColor
+    }
+    func changeColor() -> UIColor{
+
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
